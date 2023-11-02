b7c2513545f079133b092f2d9bcc62ccad14dc1f
uran0sH <huangwenyuu@outlook.com>
2023-11-02 12:58:46 -0400

    ch8

--- os/src/syscall/sync.rs
+++ os/src/syscall/sync.rs
@@ -2,6 +2,7 @@ use crate::sync::{Condvar, Mutex, MutexBlocking, MutexSpin, Semaphore};
 use crate::task::{block_current_and_run_next, current_process, current_task};
 use crate::timer::{add_timer, get_time_ms};
 use alloc::sync::Arc;
+use alloc::vec::Vec;
 /// sleep syscall
 pub fn sys_sleep(ms: usize) -> isize {
     trace!(
@@ -52,6 +53,15 @@ pub fn sys_mutex_create(blocking: bool) -> isize {
         id as isize
     } else {
         process_inner.mutex_list.push(mutex);
+
+        if process_inner.mutex_need.is_empty() {
+            process_inner.mutex_need.push(Vec::new());
+            process_inner.mutex_alloc.push(Vec::new());
+        }
+
+        process_inner.mutex_need[0].push(0);
+        process_inner.mutex_alloc[0].push(0);
+
         process_inner.mutex_list.len() as isize - 1
     }
 }
@@ -70,9 +80,13 @@ pub fn sys_mutex_lock(mutex_id: usize) -> isize {
     );
     let process = current_process();
     let process_inner = process.inner_exclusive_access();
+    let is_enabled_detection = process_inner.deadlock_detection;
     let mutex = Arc::clone(process_inner.mutex_list[mutex_id].as_ref().unwrap());
     drop(process_inner);
     drop(process);
+    if is_enabled_detection && mutex.is_locked() {
+        return -0xdead;
+    }
     mutex.lock();
     0
 }
@@ -125,48 +139,114 @@ pub fn sys_semaphore_create(res_count: usize) -> isize {
         process_inner
             .semaphore_list
             .push(Some(Arc::new(Semaphore::new(res_count))));
+
+        if process_inner.semaphore_need.is_empty() {
+            process_inner.semaphore_need.push(Vec::new());
+            process_inner.semaphore_alloc.push(Vec::new());
+        }
+    
+        process_inner.semaphore_alloc[0].push(0);
+        process_inner.semaphore_need[0].push(0);
+
         process_inner.semaphore_list.len() - 1
     };
     id as isize
 }
 /// semaphore up syscall
 pub fn sys_semaphore_up(sem_id: usize) -> isize {
+    let tid = current_task()
+        .unwrap()
+        .inner_exclusive_access()
+        .res
+        .as_ref()
+        .unwrap()
+        .tid;
     trace!(
         "kernel:pid[{}] tid[{}] sys_semaphore_up",
         current_task().unwrap().process.upgrade().unwrap().getpid(),
-        current_task()
-            .unwrap()
-            .inner_exclusive_access()
-            .res
-            .as_ref()
-            .unwrap()
-            .tid
+        tid
     );
     let process = current_process();
-    let process_inner = process.inner_exclusive_access();
+    let mut process_inner = process.inner_exclusive_access();
     let sem = Arc::clone(process_inner.semaphore_list[sem_id].as_ref().unwrap());
+    process_inner.finished[tid] = true;
     drop(process_inner);
     sem.up();
     0
 }
 /// semaphore down syscall
 pub fn sys_semaphore_down(sem_id: usize) -> isize {
+    let tid = current_task()
+        .unwrap()
+        .inner_exclusive_access()
+        .res
+        .as_ref()
+        .unwrap()
+        .tid;
     trace!(
         "kernel:pid[{}] tid[{}] sys_semaphore_down",
         current_task().unwrap().process.upgrade().unwrap().getpid(),
-        current_task()
-            .unwrap()
-            .inner_exclusive_access()
-            .res
-            .as_ref()
-            .unwrap()
-            .tid
+        tid
     );
     let process = current_process();
-    let process_inner = process.inner_exclusive_access();
+    let mut process_inner = process.inner_exclusive_access();
     let sem = Arc::clone(process_inner.semaphore_list[sem_id].as_ref().unwrap());
+
+    process_inner.semaphore_need[tid][sem_id] += 1;
+    let mut found = false;
+    if process_inner.is_dead_lock_detect_enabled() {
+        for (_tid, is_finish) in process_inner.finished.iter().enumerate() {
+            if *is_finish {
+                // debug!("tid[{}] finished", _tid);
+                continue;
+            } else {
+                let mut ok = true;
+                for (_sem_id, need) in process_inner.semaphore_need[_tid].iter().enumerate() {
+                    if _sem_id == 0 {
+                        continue;
+                    }
+
+                    let sem_count = process_inner.semaphore_list[_sem_id]
+                        .as_ref()
+                        .unwrap()
+                        .sem_count();
+
+                    let mut res_num = sem_count;
+                    if res_num < 0 {
+                        res_num = 0;
+                    }
+
+                    debug!(
+                        "tid=={},sem_id=={},need = {:?},count = {:?}",
+                        _tid, _sem_id, *need, res_num
+                    );
+
+                    if *need <= res_num {
+                        continue;
+                    } else {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if ok {
+                    found = true;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+        if !found {
+            return -0xDEAD;
+        }
+    }
     drop(process_inner);
     sem.down();
+    let process = current_process();
+    let mut process_inner = process.inner_exclusive_access();
+
+    process_inner.semaphore_need[tid][sem_id] -= 1;
     0
 }
 /// condvar create syscall
@@ -245,7 +325,14 @@ pub fn sys_condvar_wait(condvar_id: usize, mutex_id: usize) -> isize {
 /// enable deadlock detection syscall
 ///
 /// YOUR JOB: Implement deadlock detection, but might not all in this syscall
-pub fn sys_enable_deadlock_detect(_enabled: usize) -> isize {
+pub fn sys_enable_deadlock_detect(enabled: usize) -> isize {
     trace!("kernel: sys_enable_deadlock_detect NOT IMPLEMENTED");
-    -1
+    let current_process = current_process();
+    let mut lock = current_process.inner_exclusive_access();
+    lock.deadlock_detection = match enabled {
+        0 => false,
+        1 => true,
+        _ => return -1,
+    };
+    0
 }
